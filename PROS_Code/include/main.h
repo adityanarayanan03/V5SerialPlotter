4739871274aa809c0d3e8ae08bd18d9cf126c23a
Aditya Narayanan <aditya.narayanan03@gmail.com>
2020-04-10 19:53:08 -0500

    Backtracked to update Okapi to 4.0

--- PROS_Code/include/main.h
+++ PROS_Code/include/main.h
@@ -4,7 +4,7 @@
  * Contains common definitions and header files used throughout your PROS
  * project.
  *
- * Copyright (c) 2017-2018, Purdue University ACM SIGBots.
+ * Copyright (c) 2017-2020, Purdue University ACM SIGBots.
  * All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -39,7 +39,7 @@
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
@@ -52,7 +52,7 @@
  */
 // using namespace pros;
 // using namespace pros::literals;
-// using namespace okapi;
+using namespace okapi;
 
 /**
  * Prototypes for the competition control tasks are redefined here to ensure
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,30 +1,48 @@
 import serial
 import matplotlib.pyplot as plt
+import time
 
-dataStream = serial.Serial('COM12', baudrate = 115200,timeout = 1)
+#Creating instance of serial class for data input from V5 Brain
+dataStream = serial.Serial('COM12', baudrate = 115200,timeout = 1) 
 
+#Empty lists to hold data
 distances = []
 commandVelocity = []
 actualVelocity = []
 
-for i in range (100):
+#Creating timer for loop runtime
+startTime = time.time()
+
+#Starting loop with 5.0 second time condition
+while(time.time() - startTime < 5.0):
+
+    #Giving user a way to monitor time
+    print(startTime - time.time())
+
+    #Runs when data is available
     if (dataStream.is_open):
+
+        #unpacks and decodes serial lines
         incomingDatum = dataStream.readline()
         incomingDatum = incomingDatum.decode('utf-8')
         incomingDatum = incomingDatum.strip('\n')
-        incomingDatum = incomingDatum[6:]
-        print(incomingDatum)
-        (currentDist, currentActualVel, currentCommandVel) = incomingDatum.split(',')
+        incomingDatum = incomingDatum[6:] #6 is unique to this situation. It might not be necessary at all.
 
+        #splits incoming values at the comma (3 required), packs into a tuple
+        (currentDist, currentCommandVel, currentActualVel) = incomingDatum.split(',')
+
+        #Typecast string values into float
         currentDist = float(currentDist)
         currentActualVel = float(currentActualVel)
         currentCommandVel = float(currentCommandVel)
         
-        distances.append(currentDist)
+        #Add datum to various lists
+        distances.append(time.time() - startTime)
         commandVelocity.append(currentCommandVel)
         actualVelocity.append(currentActualVel)
 
+#Using matplotlib pyplot to create graphic
 plt.figure(1)
-plt.plot(distances, commandVelocity)
-plt.plot(distances, actualVelocity)
+plt.plot(distances, commandVelocity, color = 'blue')
+plt.plot(distances,actualVelocity, color = 'red')
 plt.show()
