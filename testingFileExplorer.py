31ee0bf8ae33d93703fbb83537f80f96d69aeb7b
Aditya Narayanan <aditya.narayanan03@gmail.com>
2020-06-06 21:33:28 -0500

    deleted unnecessary file

--- testingFileExplorer.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = filedialog.askdirectory()# show an "Open" dialog box and return the path to the selected file
-print(filename)
